c1a9e07c5e570e2dda7120de50014e1187f68f42
Dhananjay Maurya <dhananjaykumar880@gmail.com>
2022-08-14 23:54:53 +0530

    +updated path

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
+import { BASE_URL } from "../../constant";
 import Header from "../header/Header";
 import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
-    <Router>
+    <Router basename={BASE_URL}>
       <Header />
       <Container fluid>
         <AppRoutes />
--- src/redux/effects/product.ts
+++ src/redux/effects/product.ts
@@ -3,12 +3,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const getProductCategory = createAsyncThunk(
   "product/category",
   async () => {
-    const response = await fetch("/testdata/category.json");
+    const response = await fetch("testdata/category.json");
     return response.json();
   }
 );
 
 export const getProductList = createAsyncThunk("product/list", async () => {
-  const response = await fetch("/testdata/products.json");
+  const response = await fetch("testdata/products.json");
   return response.json();
 });
